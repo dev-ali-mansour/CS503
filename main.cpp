b996e0a60fe6225aaf00dc6e4c7121c3c1261eb7
Ali Mansour <dev.ali.mansour@gmail.com>
2024-03-09 04:26:53 +0200

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+    // Single Comment
+    /* This is Multiline Comment
+       You can type comments on multiple lines
+       You can type comment also here
+     */
+    /*std::cout << "Hello, World!" << std::endl;
+    std::cout << "Hello, World!" << std::endl;*/
+//    cout << "Hello, Ahmed! ";
+//    cout << "Welcome to C++!";
+
+//    cout << "Hello, Ahmed! " << "Welcome To C++!";
+    cout << "Hello, Ahmed!" << endl << "Welcome To C++!";
+    cout<<endl;
+    cout<<"Enter your name\n";
+    cout<<"Your name here"<<endl;
+    cout<<"Your age:\tEnter here\\"<<endl;
+    cout<<"Ahmed said \"C++ is intersting\"";
+
+
+    return 0;
+}
