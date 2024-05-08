68b6d04c16c770ba000694e32a97b33d914f1ac4
Ali Mansour <dev.ali.mansour@gmail.com>
2024-05-08 23:18:10 +0300

    feat: Create increment_decrement.cpp.

---
+++ lectures/lect_02/increment_decrement.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    /*
+     Pre-increment:     ++count     first increments the value of count by 1, and then use the new value of count in the expression
+     Post-increment:    count++    first use the current value of count , and then increments the value of count by 1
+     Pre-decrement:     --count     first decrements the value of count by 1, and then use the new value of count in the expression
+     Post-increment:    count--    first use the current value of count , and then decrements the value of count by 1
+     */
+
+    int x = 5;
+    int y = ++x;
+    cout << "the value of y is: " << y << endl;
+    cout << "the new value of x is: " << x << endl;
+
+    int c = 5;
+    int d = c++;
+    cout << "the value of d is: " << d << endl;
+    cout << "the new value of c is: " << c << endl;
+    cout << 3 / 2 + 5.5;
+    return 0;
+}
