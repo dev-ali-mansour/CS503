48521cafb6a65de1764e90e58735ed888237059a
Ali Mansour <dev.ali.mansour@gmail.com>
2024-05-09 00:24:57 +0300

    feat: Create question_sample_2.cpp.

---
+++ lectures/lect_03/question_sample_2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int x;
+    cout << "Enter an integer number: ";
+    cin >> x;
+    if (x > 100)
+        cout << "This number is greater than 100\n";
+    else
+        cout << "This number is less than 100\n";
+    return 0;
+}
