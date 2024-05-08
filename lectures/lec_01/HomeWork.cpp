a58fa07d400e1b2dc8600bf007e926274124a527
Ali Mansour <dev.ali.mansour@gmail.com>
2024-05-08 20:53:45 +0300

    feat: Create HomeWork.cpp.

---
+++ lectures/lec_01/HomeWork.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int integer1, integer2, sum;
+    cout << "Enter the first integer\n";
+    cin >> integer1;
+    cout << "Enter the second integer\n";
+    cin >> integer2;
+    sum = integer1 + integer2;
+    cout << "The sum is " << sum << endl;
+
+    return 0;
+}
