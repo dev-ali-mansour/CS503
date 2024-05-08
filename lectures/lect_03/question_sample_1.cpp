d0e58b72888b75ee6908a9198c3da06093cc9db4
Ali Mansour <dev.ali.mansour@gmail.com>
2024-05-09 00:14:00 +0300

    feat: Create question_sample_1.cpp.

---
+++ lectures/lect_03/question_sample_1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    long number = 0.0;
+
+    cout << "Please enter an integer number" << endl;
+    cin >> number;
+    if (number % 2 != 0)
+        cout << "This is an even number";
+
+    return 0;
+}
