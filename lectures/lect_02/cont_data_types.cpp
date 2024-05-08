3f5f185de2a6211d0fac750a52bf81eed5794a94
Ali Mansour <dev.ali.mansour@gmail.com>
2024-05-08 22:01:55 +0300

    feat: Create cont_data_types.cpp.

---
+++ lectures/lect_02/cont_data_types.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // Integer Types
+    /*
+     NOTE: The size of type int is system dependent. In contrast,types long and
+            short have fixed sizes no matter what system is used.
+            int in 32 bits system is 2 bytes
+            int in 64 bits system is 4 bytes
+            long is 4 bytes
+     */
+
+    int x;
+    long int var1; // is equivalent to long var1;
+
+    // Floating Types
+    /*
+     They have both an integer part (to the left of the decimal point) and
+     a fractional part (to the right of it).
+
+     Note: The maximum number of significant digits is called "The precision".
+     That is the number of decimal places.
+
+     Float Type occupies 4 bytes in memory with a precision of 7 digits.
+     Double Type occupies 8 bytes in memory with a precision of 15.
+
+     In C++, floating-point numbers are considered of type double by default.
+     Therefore, if you use the data type float, certain compilers might give you
+     a warning message, such as “truncation from double to float”.
+     To avoid such warning messages, you should use the double data type.
+     */
+    float temp;
+    double temp2 = 64.25;
+
+    const float CONVERSION = 2.5f; // to avoid truncation from double to float.
+
+
+
+    return 0;
+}
