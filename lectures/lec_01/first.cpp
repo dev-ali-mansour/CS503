0e90033c1231fd178fe88bfe660f97a8dd310ce0
Ali Mansour <dev.ali.mansour@gmail.com>
2024-05-08 20:45:24 +0300

    feat: Create first.cpp.

--- lectures/lec_01/first.cpp
+++ lectures/lec_01/first.cpp
@@ -1,7 +1,18 @@
 #include <iostream>
+
 using namespace std;
 
 int main() {
     cout << "Welcome to C++!";
-    return 0;
+
+    //Printing a line with multiple statements
+    cout << "Welome ";//Note the space after Welcome
+    cout << "to C++!\n ";
+
+    // Printing multiple lines with a single
+    cout << "Welcome\n to\n C++!\n";
+
+    cout<< "welcome to C++"<< endl; //write endl without "" and it is read end line
+
+    return 0;//indicate the program ended successfully
 }
