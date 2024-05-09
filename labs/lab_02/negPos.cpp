3cf97e40bf8f71f28f02de9ca381db826e02ba30
Ali Mansour <dev.ali.mansour@gmail.com>
2024-05-09 14:08:15 +0300

    feat: Create negPos.cpp.

---
+++ labs/lab_02/negPos.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int x;
+    cout<<"please enter a  number: ";
+    cin>>x;
+    if(x>0)
+            cout<<"+ve";
+      else if (x<0)
+             cout<<"-ve";
+             else 
+             cout<<"This is a ZERO";
+          return 0;
+}
+
