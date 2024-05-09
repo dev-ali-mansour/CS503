77bb03d0994c26394230742bfe765f0f8b7201aa
Ali Mansour <dev.ali.mansour@gmail.com>
2024-05-09 12:39:26 +0300

    feat: Create switch_score.cpp.

---
+++ lectures/lec_04/switch_score.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int score;
+    char grade;
+
+    cout << "Enter your score\n";
+    cin >> score;
+
+    switch (score / 10) {
+        case 0:
+        case 1:
+        case 2:
+        case 3:
+        case 4:
+        case 5:
+            grade = 'F';
+            break;
+        case 6:
+            grade = 'E';
+            break;
+        case 7:
+            grade = 'C';
+            break;
+        case 8:
+            grade = 'B';
+            break;
+        case 9:
+            grade = 'A';
+            break;
+        default:
+            cout << "Invalid test score." << endl;
+    }
+
+    cout << "The grade is " << grade;
+
+    return 0;
+}
