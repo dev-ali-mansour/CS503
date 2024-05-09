9bbb7c1ba4f2454d62eaae38275d683ec19daf8b
Ali Mansour <dev.ali.mansour@gmail.com>
2024-05-09 14:06:15 +0300

    feat: Create evenOdd.cpp.

---
+++ labs/lab_02/evenOdd.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int x;
+    cout<<"please enter a  number: ";
+    cin>>x;
+    if(x%2==0)
+            cout<<"even";
+      else
+             cout<<"odd";
+          return 0;
+}
+
