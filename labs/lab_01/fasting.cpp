15c58508ae78ae91aa75b90b68c279f48858033f
Ali Mansour <dev.ali.mansour@gmail.com>
2024-05-09 13:15:36 +0300

    feat: Create fasting.cpp.

---
+++ labs/lab_01/fasting.cpp
@@ -0,0 +1,13 @@
+# include <iostream>
+using namespace std;
+int main()
+{
+	int age;
+	cout<<"Enter a number: "<<endl;
+	cin>>age;
+	if (age>10)
+	cout << "Yes!!"<<endl;  //then
+	else
+	cout <<"No!!"<< endl;
+	return 0;
+}
