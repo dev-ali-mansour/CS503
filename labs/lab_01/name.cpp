08b157a8e99676acb1e22a2e11edb2065d90e0fd
Ali Mansour <dev.ali.mansour@gmail.com>
2024-05-09 13:14:34 +0300

    feat: Create name.cpp.

---
+++ labs/lab_01/name.cpp
@@ -0,0 +1,18 @@
+//Program to print "Hello name".
+# include <iostream>
+# include <string>
+using namespace std;
+int main()
+{
+	string name;
+	cout<<"Please enter your name: ";
+	cin>> name;
+	/*cout<<"Hello ";
+	cout<<name;
+	cout<<"!!!";
+	cout<<endl;*/
+	cout<< "Hello "<< name<<"!!!"<< endl;
+	system ("PAUSE");
+
+return 0;
+}
