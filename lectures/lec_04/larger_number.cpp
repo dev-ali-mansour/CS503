fcaf84844f6428671bec6ac89a73295ee489b04b
Ali Mansour <dev.ali.mansour@gmail.com>
2024-05-09 11:51:24 +0300

    feat: Create larger_number.cpp.

---
+++ lectures/lec_04/larger_number.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int num1, num2;
+    cout << "Enter any two integers:";
+    cin >> num1 >> num2;
+    cout << endl;
+    cout << "The two integers are: " << num1 << " and " << num2 << endl;
+
+    if (num1 > num2) cout << "The larger number is " << num1 << endl;
+    else if (num2 > num1)cout << "The larger number is " << num2 << endl;
+    else cout << "Both numbers are equal." << endl;
+
+    return 0;
+}
