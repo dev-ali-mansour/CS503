0bc869595366b5bb50e828cba4e64ab2335400c1
Ali Mansour <dev.ali.mansour@gmail.com>
2024-05-09 21:02:18 +0300

    chore: Update decrement comment In increment_decrement.cpp.

--- lectures/lect_02/increment_decrement.cpp
+++ lectures/lect_02/increment_decrement.cpp
@@ -7,7 +7,7 @@ int main() {
      Pre-increment:     ++count     first increments the value of count by 1, and then use the new value of count in the expression
      Post-increment:    count++    first use the current value of count , and then increments the value of count by 1
      Pre-decrement:     --count     first decrements the value of count by 1, and then use the new value of count in the expression
-     Post-increment:    count--    first use the current value of count , and then decrements the value of count by 1
+     Post-decrement:    count--    first use the current value of count , and then decrements the value of count by 1
      */
 
     int x = 5;
