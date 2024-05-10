8d3f0a1952c7af5b281c78f246a49b8d3744e465
Ali Mansour <dev.ali.mansour@gmail.com>
2024-05-10 09:28:14 +0300

    feat: for_compound_statement.cpp.

---
+++ lectures/lec_05/for_compound_statement.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    for (int i = 1; i <= 5; i++) {
+        cout << "Hello!" << endl;
+        cout << "*" << endl;
+    } //end of for
+    return 0;
+}
