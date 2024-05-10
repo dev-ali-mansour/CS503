9bc475d84c3c62ad5c80a68407f4973ae8ddee2e
Ali Mansour <dev.ali.mansour@gmail.com>
2024-05-10 09:40:54 +0300

    feat: Create for_loop_with_steps.cpp.

---
+++ lectures/lec_05/for_loop_with_steps.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    for (int i = 1; i <= 20; i = i + 2) //2 تزويدالمتغير بـ
+        cout << " " << i; //a simple statement
+    cout << endl; //will be printed once only!
+    return 0;
+}
