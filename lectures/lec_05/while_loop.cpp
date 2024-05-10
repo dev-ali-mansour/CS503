8c5e49032bbc9289f920da6a940118ae33327954
Ali Mansour <dev.ali.mansour@gmail.com>
2024-05-10 09:57:40 +0300

    feat: Create while_loop.cpp.

---
+++ lectures/lec_05/while_loop.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int i = 0;
+    while (i < 10) {
+        cout << i << " ";
+        i++;
+    }
+    cout << endl; //out of the loop
+    return 0;
+}
