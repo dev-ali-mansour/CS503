2da47c20fb4949fd42a1084fd98aeaa6ca05a9d2
Ali Mansour <dev.ali.mansour@gmail.com>
2024-05-10 10:49:25 +0300

    feat: Create question_sample_1_e.cpp.

---
+++ lectures/lec_05/question_sample_1_e.cpp
@@ -0,0 +1,16 @@
+// What is the output of the following C++ code?
+#include <iostream>
+using namespace std;
+int main() {
+    int num = 1;
+    while (num < 10){
+        cout << num << " ";
+        num = num + 2; }
+    cout << endl;
+}
+
+/*
+Output:
+1 3 5 7 9
+
+ */
