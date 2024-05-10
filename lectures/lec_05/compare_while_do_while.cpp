c0f142747902f5297ca4e018a81b9f75dcbb9f2a
Ali Mansour <dev.ali.mansour@gmail.com>
2024-05-10 10:11:35 +0300

    feat: Create compare_while_do_while.cpp.

--- lectures/lec_05/compare_while_do_while.cpp
+++ lectures/lec_05/compare_while_do_while.cpp
@@ -18,3 +18,15 @@ int main() {
     cout << endl;
     return 0;
 }
+
+/*
+ All the three loops have their place in C++.
+If you know, or the program can determine in advance, the number of
+repetitions needed, the for loop is the correct choice.
+If you do not know, and the program cannot determine in advance the
+number of repetitions needed, and it could be zero, the while loop is
+the right choice.
+If you do not know, and the program cannot determine in advance the
+number of repetitions needed, and it is at least once, the do...while
+loop is the right choice.
+ */
