39c761322a468af4e3bf0e65859e9217b9cbcd2a
Ali Mansour <dev.ali.mansour@gmail.com>
2024-05-10 15:03:12 +0300

    feat: Create question_sample_2.cpp.

---
+++ lectures/lec_07/question_sample_2.cpp
@@ -0,0 +1,11 @@
+/*
+Write the definition of a void function that takes as input a decimal
+number and outputs 3 times the value of the decimal number.
+ */
+#include <iostream>
+
+using namespace std;
+
+void print3Times(int x) {
+    cout << 3 * x;
+}
