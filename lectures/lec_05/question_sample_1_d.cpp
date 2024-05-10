7c86926db7b620b943c96ab9b1b70bedba358c5c
Ali Mansour <dev.ali.mansour@gmail.com>
2024-05-10 10:47:05 +0300

    feat: Create question_sample_1_d.cpp.

---
+++ lectures/lec_05/question_sample_1_d.cpp
@@ -0,0 +1,13 @@
+// What is the output of the following C++ code?
+#include <iostream>
+using namespace std;
+int main() {
+    int num = 5;
+    while (num > 5)
+        num = num + 2;
+    cout << num << endl;
+}
+
+/*
+Empty Output
+ */
