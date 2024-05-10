82a09be4238be0cd2c88629d538236315ee199a1
Ali Mansour <dev.ali.mansour@gmail.com>
2024-05-10 09:26:05 +0300

    feat: for_loop.cpp.

---
+++ lectures/lec_05/for_loop.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    for (int i = 0; i < 10; i++) cout << i << " "; //only one statement because there is no {}
+    cout << endl; //out of the loop
+    return 0;
+}
