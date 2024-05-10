87b085482b0563388e3baed5f8b541e21e12c027
Ali Mansour <dev.ali.mansour@gmail.com>
2024-05-10 09:42:04 +0300

    feat: Create for_wrong_init.cpp.

---
+++ lectures/lec_05/for_wrong_init.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    for (int i = 10; i <= 9; i++)
+        cout << i << " "; //won’t execute
+    cout << "**********";
+    return 0;
+}
