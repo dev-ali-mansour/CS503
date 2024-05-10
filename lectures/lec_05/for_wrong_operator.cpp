4b9323d22e3599e9f051bd5df93a4092cc1020b4
Ali Mansour <dev.ali.mansour@gmail.com>
2024-05-10 09:42:53 +0300

    feat: Create for_wrong_operator.cpp.

---
+++ lectures/lec_05/for_wrong_operator.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    for (int i = 9; i >= 10; i--)
+        cout << i << " "; //won’t execute
+    cout << "***********";
+    return 0;
+}
