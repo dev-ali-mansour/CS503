44a34f670bfd3fae617a0228de8b1bfc9ee03831
Ali Mansour <dev.ali.mansour@gmail.com>
2024-05-10 10:02:54 +0300

    feat: Create do_while_loop.cpp.

--- lectures/lec_05/do_while_loop.cpp
+++ lectures/lec_05/do_while_loop.cpp
@@ -10,3 +10,11 @@ int main() {
     } while (i <= 20);
     return 0;
 }
+
+/*
+ Note:
+Because the while and for loops both have entry conditions, these loops
+may never activate.
+The do...while loop, on the other hand, has an exit condition and therefore
+always executes the statement at least once.
+ */
