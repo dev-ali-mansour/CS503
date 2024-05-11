8807051bb56ebadae72bf86c6f163cc9035fa980
Ali Mansour <dev.ali.mansour@gmail.com>
2024-05-11 18:12:27 +0300

    feat: Create two_dimen_array_print.cpp.

--- lectures/lec_09/two_dimen_array_print.cpp
+++ lectures/lec_09/two_dimen_array_print.cpp
@@ -2,13 +2,13 @@
 
 using namespace std;
 
-int main(){
+int main() {
     const int NUMBER_OF_ROWS = 7;
     const int NUMBER_OF_COLUMNS = 6;
     int matrix[NUMBER_OF_ROWS][NUMBER_OF_COLUMNS];
 
     for (int row = 0; row < NUMBER_OF_ROWS; row++)
         for (int col = 0; col < NUMBER_OF_COLUMNS; col++)
-            matrix[row][col] = 0;
+            cout << matrix[row][col] << " ";
 
 }
