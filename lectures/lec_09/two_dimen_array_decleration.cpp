5163b1cb2a0f627188ecd553bf4410fac59fcbca
Ali Mansour <dev.ali.mansour@gmail.com>
2024-05-11 18:06:15 +0300

    feat: Create two_dimen_array_init.cpp.

---
+++ lectures/lec_09/two_dimen_array_decleration.cpp
@@ -0,0 +1,6 @@
+
+int main(){
+    const int NUMBER_OF_ROWS = 7;
+    const int NUMBER_OF_COLUMNS = 6;
+    int matrix[NUMBER_OF_ROWS][NUMBER_OF_COLUMNS];
+}
--- lectures/lec_09/two_dimen_array_init.cpp
+++ lectures/lec_09/two_dimen_array_init.cpp
@@ -6,4 +6,8 @@ int main(){
     const int NUMBER_OF_ROWS = 7;
     const int NUMBER_OF_COLUMNS = 6;
     int matrix[NUMBER_OF_ROWS][NUMBER_OF_COLUMNS];
+    int row = 5, col;
+    for (col = 0; col < NUMBER_OF_COLUMNS; col++)
+        matrix[row][col] = 0;
+
 }
