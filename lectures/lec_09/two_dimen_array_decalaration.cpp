8fc1c84d347f64923ecc8a16fe178ea104461a85
Ali Mansour <dev.ali.mansour@gmail.com>
2024-05-11 18:02:52 +0300

    feat: Create two_dimen_array_decalaration.cpp.

---
+++ lectures/lec_09/two_dimen_array_decalaration.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    const int NUMBER_OF_ROWS = 7;
+    const int NUMBER_OF_COLUMNS = 6;
+    int matrix[NUMBER_OF_ROWS][NUMBER_OF_COLUMNS];
+}
