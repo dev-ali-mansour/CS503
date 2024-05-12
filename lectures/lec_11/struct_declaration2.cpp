ee1f4eec7d9a75a28c52a2f5bc5e6d39883b830b
Ali Mansour <dev.ali.mansour@gmail.com>
2024-05-12 19:43:01 +0300

    feat: Create struct_declaration2.cpp.

---
+++ lectures/lec_11/struct_declaration2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+struct student2Type {
+    string firstName;
+    string lastName;
+    char courseGrade;
+    int testScore;
+    int programmingScore;
+    double GPA;
+} tempStudent;
+
+int main() {
+    cout<<"These statements define the struct studentType and\n"
+          "declare tempStudent to be a variable of type studentType";
+    return 0;
+};
