4b255dc769120ec3b366ffb831d4a2aac6eae066
Ali Mansour <dev.ali.mansour@gmail.com>
2024-05-12 19:37:59 +0300

    feat: Create struct_declaration.cpp.

---
+++ lectures/lec_11/struct_declaration.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+struct studentType {
+    string firstName;
+    string lastName;
+    char courseGrade;
+    int testScore;
+    int programmingScore;
+    double GPA;
+};
+
+int main() {
+    studentType newStudent;
+    studentType student;
+
+    return 0;
+}
