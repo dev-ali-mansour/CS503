bb4201812f28544d4d4daafe39df07dc17e3bcb9
Ali Mansour <dev.ali.mansour@gmail.com>
2024-05-13 05:38:53 +0300

    feat: Create Die Example Of Hiding Implementation Of Class.

--- lectures/lec_12/die.h
+++ lectures/lec_12/die.h
@@ -2,13 +2,13 @@ class die {
 public:
     die();
 
-//Default constructor: Sets the default number rolled by a die to 1
+    //Default constructor: Sets the default number rolled by a die to 1
     int roll();
 
-//Function to roll a die.It uses a random number generator to randomly generate a number
-//between1and6,and stores thenumberin the instance variable num and returns thenumber
+    //Function to roll a die.It uses a random number generator to randomly generate a number
+    //between1and6,and stores the number in the instance variable num and returns the number
     int getNum() const;
-//Afunctiontoreturnthenumber on thetop faceof thedie(thevalueof theinstance variable num)
+    //A function to return the number on the top face of the die(the value of the instance variable num)
 private:
     int num;
 };
